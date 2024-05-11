9da9b8060e27fdbf8b726e4b8c7b687b97f1aa17
Anthony Wiggins <thewiggins21@gmail.com>
2024-05-11 02:12:10 -0400

    completed cat-route.js

--- models/index.js
+++ models/index.js
@@ -5,22 +5,27 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+// A product can only belong to one category, but a category can have multiple products
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
+  onDelete: 'CASCADE',
 });
 
 // Categories have many Products
+// A category can have multiple products, but a product can only belong to one category
 Category.hasMany(Product, {
   foreignKey: 'category_id',
 });
 
 // Products belongToMany Tags (through ProductTag)
+// A product can have multiple tags, and a tag can have many products
 Product.belongsToMany(Tag, {
   through: ProductTag,
   foreignKey: 'product_id',
 });
 
 // Tags belongToMany Products (through ProductTag)
+// A tag can have multiple products, and a product can have many tags
 Tag.belongsToMany(Product, {
   through: ProductTag,
   foreignKey: 'tag_id',
