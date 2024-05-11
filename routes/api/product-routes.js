25eebe2669ad6922936dd1f293cfdc68d4c6e000
Anthony Wiggins <thewiggins21@gmail.com>
2024-05-11 03:17:32 -0400

    completed pro-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,39 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  try {
+    const products = await Product.findAll({
+      // be sure to include its associated Category and Tag data
+      include: [
+        { model: Category },
+        { model: Tag, through: ProductTag }
+      ]
+    });
+    // Send JSON response with products
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  try {
+    const products = await Product.findAll({
+      where: {
+        id: req.params.id,
+      },
+    // be sure to include its associated Category and Tag data
+      include: [{ model: Category }, { model: Tag, through: ProductTag }]
+    });
+    // Send JSON response with product
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json(err);
+  }  
 });
 
 // create new product
@@ -92,8 +116,21 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const deletedProduct = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    // Send JSON response with deleted product
+    console.log(deletedProduct);
+    res.json(deletedProduct);
+  } catch (err) {
+    console.error(err);
+    res.status(400).json(err);
+  }
 });
 
 module.exports = router;
